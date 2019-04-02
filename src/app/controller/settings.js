c07674adef38e372c4890edb521b06d75223bdda
FallInMyHand <fallinmyhand@gmail.com>
2019-04-02 14:37:36 +0300

    style changes

--- src/app/controller/settings.js
+++ src/app/controller/settings.js
@@ -14,28 +14,30 @@ define(['app/services/pubg'], function(pubgapi) {
     }
 
     function initUI() {
-        let eventBus = overwolf.windows.getMainWindow().eventBus;
-        let step1 = document.querySelector('#step1');
-        let button = step1.querySelector('#step1-check');
-        button.addEventListener('click', function() {
-            let nickname = step1.querySelector('#step1-username').value.trim();
+        if (window.overwolf) {
+            let eventBus = overwolf.windows.getMainWindow().eventBus;
+            let step1 = document.querySelector('#step1');
+            let button = step1.querySelector('#step1-check');
+            button.addEventListener('click', function() {
+                let nickname = step1.querySelector('#step1-username').value.trim();
 
-            if (nickname) {
-                (async function() {
-                    try {
-                        let accountId = await pubgapi.getAccountId(nickname);
-                        eventBus.trigger('obtaintUserData', {
-                            username: nickname,
-                            accountId: accountId
-                        });
+                if (nickname) {
+                    (async function() {
+                        try {
+                            let accountId = await pubgapi.getAccountId(nickname);
+                            eventBus.trigger('obtaintUserData', {
+                                username: nickname,
+                                accountId: accountId
+                            });
 
-                        overwolf.windows.close('settings', callback)
-                    } catch(e) {
-                        // user not found or other error
-                    }
-                }) ();
-            }
-        });
+                            overwolf.windows.close('settings', callback)
+                        } catch(e) {
+                            // user not found or other error
+                        }
+                    }) ();
+                }
+            });
+        }
 
         console.log('init ui')
     }
